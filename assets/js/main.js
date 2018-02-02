0dad0f232a2259e1c3e17770f5c272f4d1bca0cd
AlejandroTrejoCode <alejandrotrejocode@gmail.com>
2018-02-02 18:10:12 -0500

    getJSON not working
    getJSON not working on main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
 		fitToSection: false
 	});
     
+    new WOW().init();
+    
     $(window).scroll(function() {
         if ($(window).scrollTop() >= 500) {
             $("#header").addClass('coloredNav');
@@ -13,13 +15,29 @@ $(document).ready(function() {
         }
     });
     
-    $(".overlay-container").on("mouseenter", function(){
-        this.addClass("animated fadeIn");
-        this.removeClass('fadeOut');
-    });
-    $(".overlay-container").on("mouseleave", function(){
-        this.addClass("animated fadeOut");
-        this.removeClass('fadeIn');
+    $('.services-item').on('mouseover', function(){
+        console.log(this.id);
     });
     
+    $('.services-item').on('click', function(){
+        $('#services-overlay').css('width','100%');
+    });
+    
+    $('.closebtn').on('click', function(){
+        $('#services-overlay').css('width','0%');
+    });
+    
+    $.getJSON('result.json', function(data){
+        console.log(data.name);
+    });
+    
+    console.log("Ready");
 });
+
+function overlayEfectIn(id){
+    console.log(id)
+    $('#' + id).addClass('animated fadeIn'); $('#' + id).removeClass('fadeOut'); 
+}
+    function overlayEfectOut(id){
+    $('#'+id).addClass('animated fadeOut');
+}
